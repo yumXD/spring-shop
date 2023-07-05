8a044de53bd79b2b2c20d3bd711a5e34c6b82776
yumXD <qkpw99@gmail.com>
2023-07-05 15:00:00 +0900

    feat: 상품 설계하기

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,35 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@ToString
+public class Item {
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name="price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+}
