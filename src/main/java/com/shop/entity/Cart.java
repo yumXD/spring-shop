c7cfd54f983f11c37d9ac67054059b99cf6bd03c
yumXD <qkpw99@gmail.com>
2023-07-15 20:11:00 +0900

    feat: 장바구니 설계

---
+++ src/main/java/com/shop/entity/Cart.java
@@ -0,0 +1,22 @@
+package com.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+public class Cart extends BaseEntity {
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+}
