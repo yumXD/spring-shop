db286a836dd789f8e2aa4f34323c8bca38b2c7ea
yumXD <qkpw99@gmail.com>
2023-07-17 13:23:00 +0900

    feat: 주문, 주문 상품 설계 (양뱡향)

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,34 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; //주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문상태
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL
+            , orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<OrderItem> orderItems = new ArrayList<>();
+}
