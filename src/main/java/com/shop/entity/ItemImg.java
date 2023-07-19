43f613c6f8300bde11b41cc4b360409c11a6ca5e
yumXD <qkpw99@gmail.com>
2023-07-19 18:54:00 +0900

    feat: 상품 등록 기능

---
+++ src/main/java/com/shop/entity/ItemImg.java
@@ -0,0 +1,35 @@
+package com.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "item_img")
+@Getter
+@Setter
+public class ItemImg extends BaseEntity {
+
+    @Id
+    @Column(name = "item_img_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName;
+
+    private String imgUrl;
+
+    private String repImgYn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item; //단방향..
+
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl) {
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+}
---
+++ src/main/java/com/shop/service/ItemService.java
@@ -0,0 +1,43 @@
+package com.shop.service;
+
+import com.shop.dto.ItemFormDto;
+import com.shop.entity.Item;
+import com.shop.entity.ItemImg;
+import com.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    private final ItemImgService itemImgService;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+
+            if (i == 0)
+                itemImg.setRepImgYn("Y");
+            else
+                itemImg.setRepImgYn("N");
+
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+}
