548d25a554ecd5c472b0a2c32cd26bded17dd2e3
yumXD <qkpw99@gmail.com>
2023-07-20 14:55:00 +0900

    Test: 상품 등록 테스트

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -4,6 +4,9 @@ import com.shop.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 }
