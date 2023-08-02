24d458cebf83e51d330fcaa89d64ddf266ceeecc
yumXD <qkpw99@gmail.com>
2023-08-02 22:41:00 +0900

    feat: 메인화면

--- src/main/java/com/shop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.shop.repository;
 
 import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ItemRepositoryCustom {
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 }
