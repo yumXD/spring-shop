9883e3c244b2bff3004f1c79f7d67027e5d08fd9
yumXD <qkpw99@gmail.com>
2023-08-05 13:38:00 +0900

    feat: 주문 이력 조회

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
+    ItemImg findByItemIdAndRepImgYn(Long itemId, String repimgYn);
 }
--- src/main/java/com/shop/repository/OrderRepository.java
+++ src/main/java/com/shop/repository/OrderRepository.java
@@ -1,9 +1,24 @@
 package com.shop.repository;
 
 import com.shop.entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("select o from Order o " +
+            "where o.member.email = :email " +
+            "order by o.orderDate desc"
+    )
+    List<Order> findOrders(@Param("email") String email, Pageable pageable);
+
+    @Query("select count(o) from Order o " +
+            "where o.member.email = :email"
+    )
+    Long countOrder(@Param("email") String email);
 }
