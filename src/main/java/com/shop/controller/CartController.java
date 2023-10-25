d254bb28eb902c4cc469f7e568bb163365f2de11
yumXD <qkpw99@gmail.com>
2023-10-25 15:34:23 +0900

    refactor: 장바구니 관련 리펙토링

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -6,6 +6,7 @@ import com.shop.dto.CartOrderDto;
 import com.shop.service.CartService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,13 +20,25 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class CartController {
     private final CartService cartService;
 
+    @GetMapping(value = "/cart")
+    public String orderHist(Principal principal, Model model) {
+        log.info("장바구니 목록 페이지");
+        List<CartDetailDto> cartDetailList = cartService.getCartList(principal.getName());
+        model.addAttribute("cartItems", cartDetailList);
+        return "cart/cartList";
+    }
+
     @PostMapping(value = "/cart")
-    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto, BindingResult bindingResult, Principal principal) {
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto,
+                                              BindingResult bindingResult,
+                                              Principal principal) {
 
         if (bindingResult.hasErrors()) {
+            log.error("장바구니에 상품 등록 에러");
             StringBuilder sb = new StringBuilder();
             List<FieldError> fieldErrors = bindingResult.getFieldErrors();
 
@@ -41,23 +54,18 @@ public class CartController {
 
         try {
             cartItemId = cartService.addCart(cartItemDto, email);
+            log.info("장바구니에 상품 등록 성공");
         } catch (Exception e) {
+            log.error("장바구니에 상품 등록 예외처리");
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/cart")
-    public String orderHist(Principal principal, Model model) {
-        List<CartDetailDto> cartDetailList = cartService.getCartList(principal.getName());
-        model.addAttribute("cartItems", cartDetailList);
-        return "cart/cartList";
-    }
-
     @PatchMapping(value = "/cartItem/{cartItemId}")
     public @ResponseBody ResponseEntity updateCartItem(@PathVariable("cartItemId") Long cartItemId, int count, Principal principal) {
-
+        log.info("수량 count: {}", count);
         if (count <= 0) {
             return new ResponseEntity<String>("최소 1개 이상 담아주세요", HttpStatus.BAD_REQUEST);
         } else if (!cartService.validateCartItem(cartItemId, principal.getName())) {
@@ -76,7 +84,7 @@ public class CartController {
         }
 
         cartService.deleteCartItem(cartItemId);
-
+        log.info("장바구니에서 상품삭제 성공");
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
 
@@ -96,6 +104,7 @@ public class CartController {
         }
 
         Long orderId = cartService.orderCartItem(cartOrderDtoList, principal.getName());
+        log.info("장바구니에서 주문하기 성공");
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
 }
